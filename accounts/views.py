6b6c473de7e16162929f62bf844268b24fb60610
MUHAMMAD SHAHALSHAD <shahallshad@gmail.com>
2025-07-01 12:51:34 +0530

    checkout is almost done

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.contrib import messages, auth
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+
+from carts.models import Cart, CartItem
+from carts.views import _cart_id
 from .forms import RegistrationForm
 from .models import Account
 from django.contrib.auth.decorators import login_required
@@ -59,6 +62,17 @@ def user_login(request):
         
         user = auth.authenticate(email=email, password=password)
         if user is not None:
+            try:
+                cart = Cart.objects.get(cart_id = _cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+                    
+                    for item in cart_item:
+                        item.user = user
+                        item.save()
+            except:
+                pass
             auth.login(request, user)
             # messages.success(request, 'You are logged in')
             return redirect ('home')
